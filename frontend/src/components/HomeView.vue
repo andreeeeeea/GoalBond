cbc5e286bd28284896d8b3d2332fa26ec832925a
Andreea G <ghimpusorinaandreea@gmail.com>
2025-03-18 01:19:56 +0200

    Homepage Edit
    Removed placeholder image for a svg

--- frontend/src/components/HomeView.vue
+++ frontend/src/components/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home-view relative flex flex-col items-center justify-center">
     <section class="flex items-center">
-      <div class="flex items-start w-1/2 ml-64">
+      <div class="flex items-start w-1/2 ml-64 pr-32">
         <section class="relative z-10 flex flex-col justify-start items-start w-full space-y-6 text-left py-28">
           <div class="text-5xl my-1 typing-text">
             <span class="caveat-font text-gray-800">I want to... </span>
@@ -45,16 +45,13 @@
         </section>
       </div>
 
-
-      <div class="flex items-center w-1/2 mr-64 pt-10">       
+      <div class="flex items-left w-1/2 mr-64 mt-10">       
         <div class="screenshot-placeholder flex justify-center items-center pb-28">
-          <div class="relative w-2/3 transform rotate-3">
-            <img 
-              src="https://images.unsplash.com/photo-1553649033-3fbc8d0fa3cb?ixlib=rb-4.0.3" 
-              alt="Screenshot of Goals Page" 
-              class=" rounded-xl w-full h-auto" 
-              style="mask-image: linear-gradient(to bottom, black 60%, transparent 100%);" 
-            />
+          <div class="relative w-2/3 transform">
+            <svg width="500px" height="400px" viewBox="0 0 36 36" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="iconify iconify--twemoji" preserveAspectRatio="xMidYMid meet">
+              <path fill="#B03052" d="M27 8a6.98 6.98 0 0 0-2.015.298c.005-.1.015-.197.015-.298a5.998 5.998 0 0 0-11.785-1.573A5.974 5.974 0 0 0 11 6a6 6 0 1 0 0 12a5.998 5.998 0 0 0 5.785-4.428A5.975 5.975 0 0 0 19 14c.375 0 .74-.039 1.096-.104c-.058.36-.096.727-.096 1.104c0 3.865 3.135 7 7 7s7-3.135 7-7a7 7 0 0 0-7-7z"/>
+              <path fill="#D76C82" d="M31 22c-.467 0-.91.085-1.339.204c.216-.526.339-1.1.339-1.704a4.5 4.5 0 0 0-4.5-4.5a4.459 4.459 0 0 0-2.701.921A6.497 6.497 0 0 0 16.5 12a6.497 6.497 0 0 0-6.131 4.357A8 8 0 1 0 8 32h23c2.762 0 5-2.238 5-5s-2.238-5-5-5z"/>
+            </svg>
           </div>
         </div>
       </div>
